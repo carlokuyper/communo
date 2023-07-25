3526c54fff0380406e08eef77401530f903cbf9b
carlokuyper <180145@virtualwindow.co.za>
2023-07-25 15:48:42 +0200

    OnBoarding

---
+++ screens/OnBoarding.js
@@ -0,0 +1,8 @@
+https://www.google.com/search?q=react+native+onboarding&ei=B82_ZLSwF7CWhbIP_NunqAk&oq=react+native+on&gs_lp=Egxnd3Mtd2l6LXNlcnAiD3JlYWN0IG5hdGl2ZSBvbioCCAAyCBAAGIoFGJECMgUQABiABDIIEAAYigUYkQIyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAESIgTUPUEWK4GcAF4AZABAJgBogKgAa8EqgEDMi0yuAEDyAEA-AEBwgIKEAAYRxjWBBiwA-IDBBgAIEGIBgGQBgg&sclient=gws-wiz-serp
+
+https://www.npmjs.com/package/react-native-onboarding-swiper
+
+https://blog.openreplay.com/setting-up-onboarding-screens-in-react-native/
+
+https://blog.logrocket.com/how-to-create-onboarding-screens-with-react-native-viewpager/ 
+Die een!
